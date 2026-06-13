16a6f645713ca8efffd98d4e986bdd07a303d999
suryapreschool07 <suryapreschool07@gmail.com>
2026-06-13 18:41:42 +0530

    Create admin_staff_screen.dart

---
+++ lib/features/admin/screens/admin_staff_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AdminStaffScreen extends StatelessWidget {
+  const AdminStaffScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Staff'),
+      ),
+      body: const Center(
+        child: Text('Staff screen coming soon'),
+      ),
+    );
+  }
+}
